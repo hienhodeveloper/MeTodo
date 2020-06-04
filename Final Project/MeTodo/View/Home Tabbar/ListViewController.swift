1345213c9a4c0c3e4282a8712fd840b3405d09c4
hienho <hien.ho@c2c-techhub.io>
2020-06-04 20:07:12 +0700

    xcvcxvxcvxcvxcvxcv

--- Final Project/MeTodo/View/Home Tabbar/ListViewController.swift
+++ Final Project/MeTodo/View/Home Tabbar/ListViewController.swift
@@ -399,7 +399,7 @@ class ListViewController: UIViewController {
         delegate?.updateTodoTotal(incrementBy: 1)
         setTodosLabel()
         
-        MeNotificationManager.shared.addNotification(title: "\(text) - \(TaskLevel.allCases[level].text)", date: date)
+        MeNotificationManager.shared.addNotification(title: "\(text) - \(R.string.localization.level()): \(TaskLevel.allCases[level].text)", date: date)
         MeNotificationManager.shared.scheduleNotifications()
     }
     
